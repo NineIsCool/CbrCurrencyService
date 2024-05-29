23b2a4e68e64417c2ccf437567da4696e675e6da
Alexeev Nikita <nakinaki243@gmail.com>
2024-05-29 12:06:01 +0500

    init project

---
+++ src/main/java/com/example/cbrcurrencyservice/CbrCurrencyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cbrcurrencyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbrCurrencyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbrCurrencyServiceApplication.class, args);
+    }
+
+}
